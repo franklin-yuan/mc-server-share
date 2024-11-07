5e636299ef39ba4079e13b9948a4e5e960ac48a1
Franky poo <franklinyuan0205@gmail.com>
2024-11-07 20:59:55 +1300

    test

--- server1.20.4/update_pull.py
+++ server1.20.4/update_pull.py
@@ -10,6 +10,6 @@ print("Getting new files:")
 
 repo = git.Repo(config.worldpath)
 #print(repo.git.status())
-print(repo.git.execute("git pull origin main"))
+print(repo.git.execute("git clone https://github.com/franklin-yuan/mc-server-share.git"))
 
 #repo.git.execute("git checkout -b my_branch")

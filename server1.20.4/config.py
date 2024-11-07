47898bdebb8a6ae4fc8e9a5f6b755fdb99817b21
Franky poo <franklinyuan0205@gmail.com>
2024-11-07 21:35:45 +1300

    test

--- server1.20.4/config.py
+++ server1.20.4/config.py
@@ -1,4 +1,4 @@
-worldpath = r"C:\Users\frank\OneDrive\Desktop\minecraft\serverAutoShare"
+worldpath = r"../../serverAutoShare"
 #EDIT THIS ^^^ MAKE SURE THERE IS an r in front of the string
 #i.e r"<path>"
 #test

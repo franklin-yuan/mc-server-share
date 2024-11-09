58863053fa48034f3cdc24edf8c65fc685651f9a
Franky poo <franklinyuan0205@gmail.com>
2024-11-09 14:08:06 +1300

    Update from Frank

--- server1.20.4/start.bat
+++ server1.20.4/start.bat
@@ -6,9 +6,10 @@ start "" ngrok tcp 25565 --region au
 
 java -Xmx3072M -Xms3072M -jar server.jar nogui
 
+taskkill.exe /IM ngrok.exe /F 
 
 python ./update_push.py
 
-taskkill.exe /IM ngrok.exe /F 
+
 
 PAUSE

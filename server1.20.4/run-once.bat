d1276d87a8abc354d1c39fff06041be5dd4e1c81
Franky poo <franklinyuan0205@gmail.com>
2024-11-10 15:54:43 +1300

    port to bat

--- server1.20.4/run-once.bat
+++ server1.20.4/run-once.bat
@@ -6,24 +6,47 @@ set OLDDIR=%CD%
 cls
 
 
-python ./get-pip.py
+@REM python ./get-pip.py
 
-cd \
-cd \python%python_ver%\Scripts\
-pip install gitpython
-pip install ngrok
+@REM cd \
+@REM cd \python%python_ver%\Scripts\
+@REM pip install gitpython
+@REM pip install ngrok
+
+cd .. 
+git init
+git remote add origin https://github.com/franklin-yuan/mc-server-share.git
+
+echo Please select which world you would like to host. The current worlds include:
+echo Current worlds include:
+cd ..
+git branch -r
+echo Entering one that is not in the list will create a new world of that name.
+set /p world=Enter world (without the 'origin/', etc main):
+
+git checkout %world%
+
+echo Currently on:
+
+git branch
+
+git branch -M %world%
 
 chdir /d %OLDDIR% &rem restore current directory
 
-python ./get_repo.py
+@REM python ./get_repo.py
 
-call config.bat
+echo We need your ngrok authentication token (authtoken) now. Please make sure you have signed up and have your authtoken ready.
+set /p authtoken=Authtoken: 
+echo We need an email for git to use. Please enter your email: 
+set /p user_email=Email: 
+set user_email=%user_email: =%
+echo We need a username for git to use. Please enter a username: 
+set /p user_name=Username:  
+
 ngrok config add-authtoken %authtoken%
 git config --global user.email %user_email%
 git config user.email %user_name%
-
-
-
 
 
 pause
--- server1.20.4/test.bat
+++ server1.20.4/test.bat
@@ -1,16 +1,6 @@
 @echo off
 
-echo Please select which world you would like to host. The current worlds include:
-echo Current worlds include:
-cd ..
-git branch -r
-echo Entering one that is not in the list will create a new world of that name.
-set /p world=Enter world (without the 'origin/', etc main):
-
-git checkout %world%
-
-echo Currently on:
-
-git branch
-
-pause
+cd .. 
+git init
+git remote add origin https://github.com/franklin-yuan/mc-server-share.git
+git branch -M 

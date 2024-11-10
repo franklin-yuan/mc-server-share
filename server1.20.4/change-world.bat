2b8d62169cdcc40e4b7115761e7e5716d20ac695
franky <franklinyuan0205@gmail.com>
2024-11-10 20:10:24 +1300

    Update from Frank

--- server1.20.4/change-world.bat
+++ server1.20.4/change-world.bat
@@ -23,7 +23,7 @@ echo:
 
 echo Please select which world you would like to host. The current worlds include:
 echo Current worlds include:
-cd ..
+
 git branch -r
 echo Entering one that is not in the list will create a new world of that name.
 set /p world=Enter world (without the 'origin/', etc main): 

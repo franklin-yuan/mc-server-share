2660de3c8e90dd49f89a1d1635e6bc10a12e4930
thunderball <franklinyuan0205@gmail.com>
2024-12-05 23:38:00 +1300

    Update from frank

--- server1.20.4/change-world.bat
+++ server1.20.4/change-world.bat
@@ -15,10 +15,11 @@ cls
 
 cd .. 
 git init
+git lfs install
 git remote add origin https://github.com/franklin-yuan/mc-server-share.git
 git status
 git fetch --all
-git reset --hard origin/main
+git reset --hard origin/RLCraft
 
 echo:
 echo:
